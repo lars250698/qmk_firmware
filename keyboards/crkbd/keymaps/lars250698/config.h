e67fe84aabca9f7b5c8ac78d41901310935e3313
Lars Eppinger <36228556+lars250698@users.noreply.github.com>
2025-05-30 11:03:46 +0200

    add improvements for dedicated shift layout

--- keyboards/crkbd/keymaps/lars250698/config.h
+++ keyboards/crkbd/keymaps/lars250698/config.h
@@ -20,10 +20,55 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
+#define MASTER_LEFT
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_TRANSPORT_MIRROR
+#define SPLIT_MODS_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define USE_MATRIX_I2C
 
-//#define QUICK_TAP_TERM 0
-//#define TAPPING_TERM 100
+#define QUICK_TAP_TERM 100
+#define QUICK_TAP_TERM_PER_KEY
+#define TAPPING_TERM 170
+#define FLOW_TAP_TERM 150
+
+#define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
+
+// Enable auto shift instead of home row mod shifting.
+//#define AUTO_SHIFT
+
+// Enables shift optimizations for home row mods.
+// #define HRM_SHIFT
+
+// Enables dedicated shift
+#define DEDICATED_SHIFT
+
+#ifdef HRM_SHIFT
+#define TAPPING_TERM_SHIFT 140
+#define TAPPING_TERM_PER_KEY
+#define RETRO_TAPPING
+#define RETRO_TAPPING_PER_KEY
+#define PERMISSIVE_HOLD_PER_KEY
+#endif
+
+#ifdef DEDICATED_SHIFT
+#define TAPPING_TERM_PER_KEY
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define TAPPING_TERM_SHIFT 200
+#define COMBO_TERM 70
+#endif
+
+#ifdef AUTO_SHIFT
+#define AUTO_SHIFT_TIMEOUT 135
+#define RETRO_SHIFT 500
+#define NO_AUTO_SHIFT_TAB
+#endif
+
+
+#define CAPS_WORD_INVERT_ON_SHIFT
+
+#define LAYER_LOCK_IDLE_TIMEOUT 60000
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
@@ -40,4 +85,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_MATRIX_KEYPRESSES
+#   define RGB_MATRIX_SLEEP
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
+// #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+#    define ENABLE_RGB_MATRIX_CYCLE_ALL
+#    define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#    define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
 #endif
